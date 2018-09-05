0815c8fe798b86e85ac7a3b70a59997ec02a5b81
Евгений Васильев <e.vasilyev@corpwebgames.com>
2018-09-05 11:51:14 +0300

    add cpp submodule

--- SwiftCpp/CppWrapper.mm
+++ SwiftCpp/CppWrapper.mm
@@ -1,16 +1,16 @@
 
 #import <memory>
 #import "CppWrapper.h"
-#import "CppInterface.h"
+#import "CppFacade.h"
 
 @implementation CppWrapper
 
 - (int)getInt {
-    return std::make_shared<CppInterface>()->getInt();
+    return CppFacade::getCppFacadeInstance().getNewWordsCount();
 }
 
 - (NSString*)getString {
-    const std::string& string = std::make_shared<CppInterface>()->getString();
+    const std::string& string = CppFacade::getCppFacadeInstance().getStudySessionBeingWordOriginalString();
     NSString* nsString = [NSString stringWithUTF8String:string.c_str()];
     return nsString;
 }
